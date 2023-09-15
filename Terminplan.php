07f39c2e61deace58f0375e7358dd81e0cdf4793
Matthe255dev <116518249+Matthe255dev@users.noreply.github.com>
2023-09-15 19:32:43 +0200

    Upload
    Upload of index, login and Terminplan
    Upload of main.php (without login information)

---
+++ Terminplan.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Termine</title>
+</head>
+<body>
+    <h1 style="">Es tut uns leid. Dieses Feature ist leider noch nicht verfügbar.</h1>
+    <script>
+        alert("Dieses Feature ist derzeit nicht verfügbar!");
+    </script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <p>Benutzername</p>
+    <input>
+    <p>Password</p>
+    <input>
+    <button onclick="alert('Beim überprüfen ihrer Daten ist ein Fehler aufgetreten!');window.open('index.html');window.close();">Login</button>
+</body>
+</html>
